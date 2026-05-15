265fe99d06f08d61fd44a8691da34b932ec13b5c
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-05-15 15:30:05 +0200

    fix potential errors with middleware continuing execution after calling no longer being handling request
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- wallet-gateway/remote/src/middleware/jsonRpcHandler.ts
+++ wallet-gateway/remote/src/middleware/jsonRpcHandler.ts
@@ -97,7 +97,7 @@ export const jsonRpcHandler =
 
         return (req: Request, res: Response, next: NextFunction) => {
             if (req.method !== 'POST') {
-                next()
+                return next()
             }
 
             const parsed = JsonRpcRequest.safeParse(req.body)
@@ -119,7 +119,7 @@ export const jsonRpcHandler =
                     logger
                 )
 
-                res.status(status).json(response)
+                return res.status(status).json(response)
             } else {
                 const { method, params, id = null } = parsed.data
 
@@ -148,7 +148,7 @@ export const jsonRpcHandler =
                         method
                     )
 
-                    res.status(status).json(response)
+                    return res.status(status).json(response)
                 }
 
                 // TODO: validate params match the expected schema for the method
