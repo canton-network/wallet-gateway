1f8c69e0b220987651db4362f88adbf724dfaef7
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-05-19 15:18:42 +0200

    docs(wallet-sdk): add plugin docs
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

---
+++ docs/wallet-integration-guide/examples/snippets/plugin.ts
@@ -0,0 +1,32 @@
+import { SDK, SDKContext, SDKPlugin } from '@canton-network/wallet-sdk'
+
+export default async function () {
+    const sdk = (
+        await SDK.create({
+            auth: {
+                method: 'self_signed',
+                issuer: 'unsafe-auth',
+                credentials: {
+                    clientId: 'ledger-api-user',
+                    clientSecret: 'unsafe',
+                    audience: 'https://canton.network.global',
+                    scope: '',
+                },
+            },
+            ledgerClientUrl: 'http://localhost:2975',
+        })
+    ).registerPlugins({
+        myPlugin: class extends SDKPlugin {
+            // wallet-sdk plugin should always accept SDKContext
+            constructor(protected readonly ctx: SDKContext) {
+                super('myPlugin', ctx)
+            }
+
+            myMethod() {
+                console.log("It's working!", this.ctx)
+            }
+        },
+    })
+
+    sdk.myPlugin.myMethod()
+}
